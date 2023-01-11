ed5c658ee691d5706e04c58c21cbbbbd8cf4708b
jfk93-fr <85239242+jfk93-fr@users.noreply.github.com>
2023-01-11 12:48:15 +0100

    déplacement fichiers

--- MyChronoGPS.sh
+++ MyChronoGPS.sh
@@ -23,13 +23,13 @@ fi
 # création des dossiers et fichiers web
 sudo cp ./Web-Pi/index.html /var/www/html/index.html         
 sudo cp ./Web-Pi/MyChronoGPS.html /var/www/html/MyChronoGPS.html
-sudo cp ./Web-Pi/MyChronoGPS-Analysis.html /var/www/html/MyChronoGPS-Analysis.html
+sudo cp ./Web-Pi/Leaflet/MyChronoGPS-Analysis.html /var/www/html/MyChronoGPS-Analysis.html
 sudo cp ./Web-Pi/MyChronoGPS-Cmd.html /var/www/html/MyChronoGPS-Cmd.html
 sudo cp ./Web-Pi/MyChronoGPS-Dashboard.html /var/www/html/MyChronoGPS-Dashboard.html
-sudo cp ./Web-Pi/MyChronoGPS-DesignTrack.html /var/www/html/MyChronoGPS-DesignTrack.html
-sudo cp ./Web-Pi/MyChronoGPS-Live.html /var/www/html/MyChronoGPS-Live.html
+sudo cp ./Web-Pi/Leaflet/MyChronoGPS-DesignTrack.html /var/www/html/MyChronoGPS-DesignTrack.html
+sudo cp ./Web-Pi/Leaflet/MyChronoGPS-Live.html /var/www/html/MyChronoGPS-Live.html
 sudo cp ./Web-Pi/MyChronoGPS-Sessions.html /var/www/html/MyChronoGPS-Sessions.html
-sudo cp ./Web-Pi/MyChronoGPS-Tracks.html /var/www/html/MyChronoGPS-Tracks.html
+sudo cp ./Web-Pi/Leaflet/MyChronoGPS-Tracks.html /var/www/html/MyChronoGPS-Tracks.html
 
 sudo cp ./Web-Pi/ajax /var/www/html/ajax -r
 sudo cp ./Web-Pi/css /var/www/html/css -r
