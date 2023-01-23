80d3e4306193f50129339d2790f415a03c003052
jfk93-fr <85239242+jfk93-fr@users.noreply.github.com>
2023-01-23 22:20:02 +0100

    Update MyChronoGPS.sh

--- MyChronoGPS.sh
+++ MyChronoGPS.sh
@@ -64,59 +64,54 @@ then
 	if [ "$_web" != "0" ]
 	then
 		echo "It seems that the folders and web files are already installed."
-		echo "If you continue, the files will be overwritten."
-		echo "Do you want to continue ? (Y-N)"
-		read rep
-		if [ $rep = "Y" ] || [ $rep = "y" ]
-		then
-			echo "creation of folders and web files"
-			sudo cp ./Web-Pi/index.html /var/www/html/index.html         
-			sudo cp ./Web-Pi/MyChronoGPS.html /var/www/html/MyChronoGPS.html
-			sudo cp ./Web-Pi/MyChronoGPS-Cmd.html /var/www/html/MyChronoGPS-Cmd.html
-			sudo cp ./Web-Pi/MyChronoGPS-Dashboard.html /var/www/html/MyChronoGPS-Dashboard.html
-			sudo cp ./Web-Pi/MyChronoGPS-Parms.html /var/www/html/MyChronoGPS-Parms.html
-			sudo cp ./Web-Pi/MyChronoGPS-Sessions.html /var/www/html/MyChronoGPS-Sessions.html
-			
-			sudo cp ./Web-Pi/ajax /var/www/html -r
-			sudo cp ./MyChronoGPS_Paths.py /var/www/html/ajax/MyChronoGPS_Paths.py
-			
-			sudo cp ./Web-Pi/css /var/www/html -r
-			sudo cp ./Web-Pi/Icones /var/www/html -r
-			sudo cp ./Web-Pi/img /var/www/html -r
-			sudo cp ./Web-Pi/js /var/www/html -r
-			
-			sudo cp ./Web-Pi/Leaflet/html /var/www -r
-			echo "Have a GoogleMaps API key ? (Y-N)"
-			read rep
-			if [ $rep = "Y" ] || [ $rep = "y" ]
-			then
-				echo "installation of GoogleMaps instead of Leaflet"
-				sudo cp ./Web-Pi/GoogleMaps/html /var/www -r
-				echo "While editing the file,"
-				echo "Replace the value 'Your_API_Key' with the value of your GoogleMaps API key "
-				echo "press enter to continue"
-				read dummy
-				sudo nano /var/www/html/js/key.js
-				#echo "var myKey = '"$KEY"';" >> /var/www/html/js/key.js
-			fi
-			#
-			echo "changing access rights of www-data"
-			sudo chown -R www-data:www-data /var/www
-			sudo usermod -a -G www-data $USER
-			sudo chmod -R g+rwX /var/www
-			groups $USER
-			
-			if [ -d cache ]; then
-				sudo mount -t tmpfs -o size=2M,mode=0777 tmpfs $HOME/MyChronoGPS/cache
-			fi
-			if [ -d pipes ]; then
-				sudo mount -t tmpfs -o size=20M,mode=0777 tmpfs $HOME/MyChronoGPS/pipes
-			fi
-			
-			sudo chown -R www-data:www-data $HOME/MyChronoGPS
-		fi
+		echo "The files will be overwritten."
 	fi
 fi
+echo "creation of folders and web files"
+sudo cp ./Web-Pi/index.html /var/www/html/index.html         
+sudo cp ./Web-Pi/MyChronoGPS.html /var/www/html/MyChronoGPS.html
+sudo cp ./Web-Pi/MyChronoGPS-Cmd.html /var/www/html/MyChronoGPS-Cmd.html
+sudo cp ./Web-Pi/MyChronoGPS-Dashboard.html /var/www/html/MyChronoGPS-Dashboard.html
+sudo cp ./Web-Pi/MyChronoGPS-Parms.html /var/www/html/MyChronoGPS-Parms.html
+sudo cp ./Web-Pi/MyChronoGPS-Sessions.html /var/www/html/MyChronoGPS-Sessions.html
+
+sudo cp ./Web-Pi/ajax /var/www/html -r
+sudo cp ./MyChronoGPS_Paths.py /var/www/html/ajax/MyChronoGPS_Paths.py
+
+sudo cp ./Web-Pi/css /var/www/html -r
+sudo cp ./Web-Pi/Icones /var/www/html -r
+sudo cp ./Web-Pi/img /var/www/html -r
+sudo cp ./Web-Pi/js /var/www/html -r
+
+sudo cp ./Web-Pi/Leaflet/html /var/www -r
+echo "Have a GoogleMaps API key ? (Y-N)"
+read rep
+if [ $rep = "Y" ] || [ $rep = "y" ]
+then
+	echo "installation of GoogleMaps instead of Leaflet"
+	sudo cp ./Web-Pi/GoogleMaps/html /var/www -r
+	echo "While editing the file,"
+	echo "Replace the value 'Your_API_Key' with the value of your GoogleMaps API key "
+	echo "press enter to continue"
+	read dummy
+	sudo nano /var/www/html/js/key.js
+	#echo "var myKey = '"$KEY"';" >> /var/www/html/js/key.js
+fi
+#
+echo "changing access rights of www-data"
+sudo chown -R www-data:www-data /var/www
+sudo usermod -a -G www-data $USER
+sudo chmod -R g+rwX /var/www
+groups $USER
+
+if [ -d cache ]; then
+	sudo mount -t tmpfs -o size=2M,mode=0777 tmpfs $HOME/MyChronoGPS/cache
+fi
+if [ -d pipes ]; then
+	sudo mount -t tmpfs -o size=20M,mode=0777 tmpfs $HOME/MyChronoGPS/pipes
+fi
+
+sudo chown -R www-data:www-data $HOME/MyChronoGPS
 
 sudo chmod -R u+wx,g+wx,o+wx $HOME/MyChronoGPS
 
